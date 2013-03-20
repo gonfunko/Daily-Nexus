0eac8765ac2eca4347f662063c729acdb919ee0f
Aaron Dodson <gonfunko@gmail.com>
2013-03-20 12:54:10 -0700

    Added NSString category for cleaning up strings

---
+++ Daily Nexus/NSString+Additions.h
@@ -0,0 +1,14 @@
+//
+//  NSString+Additions.h
+//  Daily Nexus
+//
+//  A collection of methods to do nice things to strings
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+- (NSString *)sanitizedString;
+
+@end
