eae6eba4a537d84d1a0b10e3c5db2de3c784848e
Aaron Dodson <gonfunko@gmail.com>
2013-03-24 14:59:30 -0700

    Improved legibility of TDNArticle's HTML representation

--- Daily Nexus/TDNArticle.m
+++ Daily Nexus/TDNArticle.m
@@ -52,7 +52,7 @@
 }
 
 - (NSString *)htmlRepresentationWithHeight:(NSInteger)height {
-    NSString *htmlRepresentation = [NSString stringWithFormat:@"<html><div style=\"-webkit-column-width: 13em; -webkit-column-gap: 1em; font-family: Palatino; font-size: 12pt; color: #2b2b2b; height: %ldpx;\"><h1 style=\"font-family: Palatino; font-size: 20pt; font-weight: bold; color: #2b2b2b;\">%@</h1><h2 style=\"font-family: Palatino; font-weight: normal; font-size: 10pt; color: #b8b8b8;\">%@</h2>%@</div></html>", (long)height, self.title, [self byline], self.story];
+    NSString *htmlRepresentation = [NSString stringWithFormat:@"<html><div style=\"-webkit-column-width: 20em; -webkit-column-gap: 1.5em; font-family: Palatino; font-size: 12pt; color: #2b2b2b; height: %ldpx;\" id=\"container\"><h1 style=\"font-family: Palatino; font-size: 20pt; font-weight: bold; color: #2b2b2b;\">%@</h1><h2 style=\"font-family: Palatino; font-weight: normal; font-size: 10pt; color: #b8b8b8;\">%@</h2>%@</div></html>", (long)height, self.title, [self byline], self.rawStory];
     
     return htmlRepresentation;
 }
